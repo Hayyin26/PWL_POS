c6e180978c676033323b4472652ff5187958faf9
Hayyin26 <hayinmemon@gmail.com>
2025-03-04 17:22:53 +0700

    $fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,12 +12,12 @@ class UserController extends Controller
     {   
 
         $data = [
-            // 'username' => 'customer-1',
-            'nama' => 'Pelanggan Pertama',
-            // 'password' => Hash::make('12345'),
-            // 'level_id' => 5
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345'),
         ];
-        UserModel::where('username', 'customer-1')->update($data);
+        UserModel::create($data);
 
         $user = UserModel::all();
         return view('user', ['data' => $user]);
